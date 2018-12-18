c0ff11cfb3f2aee4bcf0a4d0139f6068e53d6112
azerbelhedi <azerbelhedide@gmail.com>
2018-12-18 08:45:56 +0100

    full  doc class

--- class.js
+++ class.js
@@ -1,18 +1,52 @@
+var reportedFiles = [] ;
+
 class Doc{
-
-    constructor(name , views){
+    
+    constructor(name , type , uni , school , path , creator , access){
         this.name = name ;
-        this.views = views ;
+        this.type = type ;
+        this.uni = uni ;
+        this.school = school ;
+        this.path = path ;
+        this.creator = creator ;
+        this.access = access ;
+        this.views = 0 ;
+        this.adds = 0 ;
+        this.html = "<div> </div>" ;
     }
 
-    printData(){
-        console.log(this.name) ;
-        console.log(this.views) ;
+    reportFile(){
+        console.log("report") ;
     }
+
+    addToMyList(user){
+        // add doc to user list 
+        console.log(`create the user : ${user} first to add files to his list` ) ;
+    }
+
+    HTML(){
+        this.html = `<div> <h1> ${this.name} </h1> <h2> ${this.type} </h2> <h2> ${this.path} </h2> <h3>${this.uni}/${this.school} </h3> </div>`
+        return this.html ;
+    }
+
+    getData(all){
+        console.log('name : ' + this.name) ;
+        console.log('views : ' + this.views) ;
+        console.log('adds : ' + this.adds ) ;
+        console.log('access : ' + this.access) ;
+        if(all == "all"){
+            console.log(this.HTML()) ;
+        }
+    }
+
 }
 
-let doc1 = new Doc("analyse test 2018 jan " , 20) ;
-let doc2 = new Doc("algebra test 2015" , 180) ;
 
-doc1.printData() ;
-doc2.printData() ;
+let file1 = new Doc("test analyse 2015 jan" , "test" , 'sousse' , 'issat' , 'analyse/test' , 'azer belhedi' , 'public') ;
+
+file1.reportFile() ;
+file1.addToMyList('wael') ;
+
+//console.log(file1.HTML() );
+
+file1.getData('all') ;
